9dcbfdb8927addcb9c5fc8d951610835650a3722
Moritz Ruth <dev@moritzruth.de>
2021-04-16 20:56:59 +0200

    Update dependencies and add impressum.txt

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import vue from "@vitejs/plugin-vue"
+import vuePlugin from "@vitejs/plugin-vue"
+import windicssPlugin from "vite-plugin-windicss"
 
 /**
  * https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
 export default {
-  plugins: [vue()]
+  plugins: [
+    vuePlugin(),
+    windicssPlugin()
+  ]
 }
---
+++ windi.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite-plugin-windicss"
+
+export default defineConfig({
+  theme: {
+    fontFamily: {
+      sans: ["Inter var", "sans-serif"]
+    }
+  }
+})
